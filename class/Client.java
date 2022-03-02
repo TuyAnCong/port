9390b398db53eebabcb133f4dfaeb509d0a5ecdb
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-03 06:39:35 +0700

    Create Client.java

---
+++ class/Client.java
@@ -0,0 +1,19 @@
+package java.pip.client
+import java.ext.*
+import java.message.client.*
+class SendClientViaLink(){
+  /*
+SendClientViaLink Class Icon Share
+*/
+start = shareLinkWithAction('portChat.message/share/${uid_group') 
+end = sayInPortChatSystem('Successfully Sended')
+shareIcon = raw('share.pngfree.com/image.png')
+}
+class sendPhoto(){
+end = sendPhotoRaw
+raw = sendMSG(end)
+}
+class Event(event){
+events_list = ["Create", "Delete", "Read Message", "etc_meta"]
+event = addEvent.permssion(["CREATE", "DELETE", "READ_MESSAGE"])
+errorEvent = system.PrintOut("Error Events! ERROR_ADD_EVENT_META")
