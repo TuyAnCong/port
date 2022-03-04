f872a89c082a0161c9ee7ec37af6b18691abcfdd
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-04 13:58:59 +0700

    Add send gif and execute file

--- class/Client.java
+++ class/Client.java
@@ -17,3 +17,9 @@ class Event(event){
 events_list = ["Create", "Delete", "Read Message", "etc_meta"]
 event = addEvent.permssion(["CREATE", "DELETE", "READ_MESSAGE"])
 errorEvent = system.PrintOut("Error Events! ERROR_ADD_EVENT_META")
+}
+class sendGIF(gif_name){
+// class action send GIF Button
+exec.file('sendGIF.php')
+data.saveMemory()
+}
